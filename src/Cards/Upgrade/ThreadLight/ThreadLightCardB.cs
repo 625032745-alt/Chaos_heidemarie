ba67f6f34ea18d518f1de04935317fff8c818821
SKY <463731963@qq.com>
2026-09-02 12:01:32 +0800

    update

---
+++ src/Cards/Upgrade/ThreadLight/ThreadLightCardB.cs
@@ -0,0 +1,41 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Keywords;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Upgrade.ThreadLight;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class ThreadLightCardB : ModCardTemplate
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(8, ValueProp.Move)];
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
+
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/ThreadLightCard.png");
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [LinkKeywords.Link, RestKeyword.REST];
+
+    public ThreadLightCardB() : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+        if (Owner.Creature.GetPower<ThreadLightCardBPower>() != null)
+            return;
+        await PowerCmd.Apply<ThreadLightCardBPower>(choiceContext, Owner.Creature,
+            1m, Owner.Creature, this);
+    }
+}
---
+++ src/Power/ThreadLightCardBPower.cs
@@ -0,0 +1,37 @@
+﻿using ChaosHeidemarie.Cards.Upgrade.ThreadLight;
+using ChaosHeidemarie.Keywords;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Power;
+
+[RegisterPower]
+public class ThreadLightCardBPower : ModPowerTemplate
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Single;
+
+    public override PowerAssetProfile AssetProfile => new(
+        IconPath: "res://ArtWorks/images/power/ThreadLightPower_Small.png",
+        BigIconPath: "res://ArtWorks/images/power/ThreadLightPower_Big.png"
+    );
+
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer,
+        CardModel? cardSource,
+        CardPlay? cardPlay)
+    {
+        if (!cardSource.Keywords.Contains(LinkKeywords.Link))
+            return 0m;
+        var player = cardPlay.Player;
+        var combatState = player.PlayerCombatState;
+        var discard = combatState.DiscardPile.Cards.Where(c => c is ThreadLightCardB).ToList();
+        var hands = combatState.Hand.Cards.Where(c => c is ThreadLightCardB).ToList();
+        if (hands.Count == 0 && discard.Count == 0) return 0m;
+        return 2m;
+    }
+}
