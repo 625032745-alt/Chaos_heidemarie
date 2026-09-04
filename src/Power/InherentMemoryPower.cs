7833eba96edc8bf0b501b54116d1947be8245ee9
SKY <463731963@qq.com>
2026-09-04 14:26:37 +0800

    update

---
+++ src/Power/InherentMemoryPower.cs
@@ -0,0 +1,64 @@
+﻿using ChaosHeidemarie.Keywords;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Power;
+
+[RegisterPower]
+public class InherentMemoryPower : ModPowerTemplate
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override PowerAssetProfile AssetProfile => new(
+        IconPath: "res://ArtWorks/images/power/InherentMemoryPower_Small.png",
+        BigIconPath: "res://ArtWorks/images/power/InherentMemoryPower_Big.png"
+    );
+
+    public override async Task AfterCardPlayedLate(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var card = cardPlay.Card;
+        await ApplyPower(choiceContext, card);
+    }
+
+    public override async Task AfterCardDiscarded(PlayerChoiceContext choiceContext, CardModel card)
+    {
+        await ApplyPower(choiceContext, card);
+    }
+
+    private async Task ApplyPower(PlayerChoiceContext choiceContext, CardModel card)
+    {
+        if (card.Keywords.Contains(LinkKeywords.Link))
+        {
+            await PowerCmd.ModifyAmount(choiceContext, this, 1m, null, card);
+        }
+    }
+
+    public override Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount,
+        Creature? applier,
+        CardModel? cardSource)
+    {
+        if (power is not InherentMemoryPower || amount <= 0)
+            return Task.CompletedTask;
+        if (power.Amount > 10)
+        {
+            SetAmount(10);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer,
+        CardModel? cardSource,
+        CardPlay? cardPlay)
+    {
+        return Owner.GetPower<InherentMemoryPower>() != null ? 3m : 0m;
+    }
+}
---
+++ src/Relics/InherentMemoryRelic.cs
@@ -0,0 +1,33 @@
+﻿using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Relics;
+
+[RegisterRelic(typeof(Content.HeidemarieRelicPool))]
+public class InherentMemoryRelic : ModRelicTemplate
+{
+    public override RelicRarity Rarity => RelicRarity.Uncommon;
+
+    public override RelicAssetProfile AssetProfile => new(
+        IconPath: "res://ArtWorks/images/relic/InherentMemoryRelic_Small.png",
+        IconOutlinePath: "res://ArtWorks/images/relic/InherentMemoryRelic_Outline.png",
+        BigIconPath: "res://ArtWorks/images/relic/InherentMemoryRelic_Big.png");
+
+    public override async Task BeforeSideTurnStart(PlayerChoiceContext choiceContext, CombatSide side,
+        IReadOnlyList<Creature> participants,
+        ICombatState combatState)
+    {
+        if (side == CombatSide.Player)
+        {
+            Flash();
+            await PowerCmd.Apply<InherentMemoryPower>(choiceContext, Owner.Creature,
+                10m, Owner.Creature, null);
+        }
+    }
+}
