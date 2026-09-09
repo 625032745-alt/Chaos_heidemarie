2f030cbdff14a1526850c7ee6719362ee08b6016
SKY <463731963@qq.com>
2026-09-09 17:36:54 +0800

    update

---
+++ src/Cards/Common/AuroraBastionCard.cs
@@ -0,0 +1,43 @@
+﻿using ChaosHeidemarie.Cards.Token;
+using ChaosHeidemarie.Content;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Common;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class AuroraBastionCard : ModCardTemplate
+{
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+
+    public override bool GainsBlock => true;
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Defend];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(4, ValueProp.Move)];
+
+    public AuroraBastionCard() : base(2, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+    }
+
+    public override decimal ModifyBlockAdditive(Creature target, decimal block, ValueProp props, CardModel? cardSource,
+        CardPlay? cardPlay)
+    {
+        if (cardSource != this)
+            return 0;
+        CardPile pile = PileType.Exhaust.GetPile(Owner);
+        var count = pile.Cards.Count(c => c is EffulgentBladeCard);
+        return count * 3;
+    }
+}
