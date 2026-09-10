cfc9c0b12c298dfb0971d02c2bd114c9cdc85e4a
SKY <463731963@qq.com>
2026-09-10 11:38:51 +0800

    update

---
+++ src/Cards/Ancient/BladeParadiseCard.cs
@@ -0,0 +1,55 @@
+﻿using ChaosHeidemarie.Content;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Ancient;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class BladeParadiseCard : ModCardTemplate
+{
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
+    public override CardAssetProfile AssetProfile => new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(15, ValueProp.Move)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain];
+    private int _discardedCount;
+    
+    
+    public BladeParadiseCard() : base(3, CardType.Attack, CardRarity.Ancient, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this,cardPlay)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    public override Task AfterCardDiscarded(PlayerChoiceContext choiceContext, CardModel card)
+    {
+        if (_discardedCount < 20)
+        {
+            _discardedCount++;
+        }
+        return Task.CompletedTask;
+    }
+
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource,
+        CardPlay? cardPlay)
+    {
+        if (cardSource != this)
+        {
+            return 0;
+        }
+        return 5 * _discardedCount;
+    }
+}
