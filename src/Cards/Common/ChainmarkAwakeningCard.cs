59343f240ac6d711365d39725ae45a8d44a55424
SKY <463731963@qq.com>
2026-09-16 11:12:57 +0800

    update

---
+++ src/Cards/Common/ChainmarkAwakeningCard.cs
@@ -0,0 +1,42 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Common;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class ChainmarkAwakeningCard : ModCardTemplate
+{
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new("ChainmarkAwakening", 1)];
+
+    public ChainmarkAwakeningCard() : base(0, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var amount = DynamicVars["ChainmarkAwakening"].BaseValue;
+        var power = Owner.Creature.GetPower<InherentMemoryPower>();
+        if (power != null)
+        {
+            await PowerCmd.ModifyAmount(choiceContext, power, amount, null, this);
+        }
+        else
+        {
+            await PowerCmd.Apply<InherentMemoryPower>(choiceContext, Owner.Creature, amount, Owner.Creature, this);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["ChainmarkAwakening"].UpgradeValueBy(1m);
+    }
+}
---
+++ src/Cards/Common/GlimmeringTetherCard.cs
@@ -0,0 +1,46 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Keywords;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Common;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class GlimmeringTetherCard : ModCardTemplate
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new("GlimmeringTether", 1)];
+    private static readonly LocString SelectFromHand = new("card_selection", "CHAOS_HEIDEMARIE_SELECT_FROM_HAND");
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+    public GlimmeringTetherCard() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CardPileCmd.Draw(choiceContext, Owner);
+        await SelectFromHandPile(choiceContext, Owner);
+        
+    }
+    
+    private async Task SelectFromHandPile(PlayerChoiceContext ctx, Player player)
+    {
+        var pile = PileType.Hand.GetPile(player);
+        var selectFrom = (from c in pile.Cards orderby c.Rarity, c.Id select c).ToList();
+        var selectCount = DynamicVars["GlimmeringTether"].IntValue;
+        var selected =
+            await CardSelectCmd.FromSimpleGrid(ctx, selectFrom, player, new CardSelectorPrefs(SelectFromHand, selectCount));
+        foreach (var card in selected)
+        {
+            card.AddKeyword(LinkKeywords.Link);
+        }
+    }
+}
