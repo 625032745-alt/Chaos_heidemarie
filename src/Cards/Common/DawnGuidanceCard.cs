a2ec3401e9c562f21f413494340ab9c030242ca0
SKY <463731963@qq.com>
2026-09-20 22:10:34 +0800

    update

--- src/Cards/Common/DawnGuidanceCard.cs
+++ src/Cards/Common/DawnGuidanceCard.cs
@@ -10,26 +10,25 @@ using STS2RitsuLib.Scaffolding.Content;
 namespace ChaosHeidemarie.Cards.Common;
 
 [RegisterCard(typeof(HeidemarieCardPool))]
-public class SilkenLightCard : ModCardTemplate
+public class DawnGuidanceCard : ModCardTemplate
 {
     public override CardAssetProfile AssetProfile =>
         new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
-
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new("SilkenLight", 1)];
-
-    public SilkenLightCard() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new("DawnGuidance", 1)];
+    
+    public DawnGuidanceCard() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
     {
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        var baseValue = DynamicVars["SilkenLight"].BaseValue;
-        await PowerCmd.Apply<SilkenLightPower>(choiceContext, Owner.Creature,
+        var baseValue = DynamicVars["DawnGuidance"].BaseValue;
+        await PowerCmd.Apply<DawnGuidancePower>(choiceContext, Owner.Creature,
             baseValue, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars["SilkenLight"].UpgradeValueBy(1);
+        DynamicVars["DawnGuidance"].UpgradeValueBy(1m);
     }
 }
---
+++ src/Power/DawnGuidancePower.cs
@@ -0,0 +1,39 @@
+﻿using ChaosHeidemarie.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Power;
+
+[RegisterPower]
+public class DawnGuidancePower : ModPowerTemplate
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    public override PowerAssetProfile AssetProfile => new(
+        IconPath: "res://ArtWorks/images/power/DawnGuidancePower_Small.png",
+        BigIconPath: "res://ArtWorks/images/power/DawnGuidancePower_Big.png"
+    );
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var card = cardPlay.Card;
+        if (card.Type == CardType.Attack)
+        {
+            await CommonUtils.AddOrModifyPower<InherentMemoryPower>(choiceContext, Owner,Amount,null);
+        }
+    }
+    
+    public override async Task BeforeSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (side == CombatSide.Player)
+        {
+            await PowerCmd.Remove(this);
+        }
+    }
+}
