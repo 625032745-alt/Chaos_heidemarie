23eefdce6d9a4de205c5716dbe2c01c531e5c4a8
SKY <463731963@qq.com>
2026-09-24 22:32:40 +0800

    update

--- src/Power/ChainResonancePower.cs
+++ src/Power/ChainResonancePower.cs
@@ -1,6 +1,9 @@
-﻿using MegaCrit.Sts2.Core.Entities.Cards;
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using STS2RitsuLib.Interop.AutoRegistration;
@@ -31,5 +34,13 @@ public class ChainResonancePower : ModPowerTemplate
         }
 
         return 0;
+    }
+
+    public override async Task BeforeSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (side ==  CombatSide.Player)
+        {
+            await PowerCmd.Remove(this);
+        }
     }
 }
