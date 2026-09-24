6cb192638ab8217e7db6b4ab8a03edc3414ffa9a
SKY <463731963@qq.com>
2026-09-24 16:12:33 +0800

    update

--- src/Cards/Rare/ChainBurstCard.cs
+++ src/Cards/Rare/ChainBurstCard.cs
@@ -2,8 +2,10 @@
 using ChaosHeidemarie.Power;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
@@ -25,22 +27,25 @@ public class ChainBurstCard : ModCardTemplate
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        var power = Owner.Creature.GetPower<InherentMemoryPower>();
-        var baseValue = DynamicVars.Damage.BaseValue;
-        if (power != null)
-        {
-            baseValue += power.Amount * 4;
-            await PowerCmd.Remove(power);
-        }
-        await DamageCmd.Attack(baseValue)
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
             .FromCard(this, cardPlay)
             .Targeting(cardPlay.Target)
             .WithHitFx("vfx/vfx_attack_slash")
             .Execute(choiceContext);
+        var power = Owner.Creature.GetPower<InherentMemoryPower>();
+        if(power != null) await PowerCmd.Remove(power);
     }
     
     protected override void OnUpgrade()
     {
         CardCmd.ApplyKeyword(this,CardKeyword.Retain);
+    }
+
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource,
+        CardPlay? cardPlay)
+    {
+        if (cardSource != this) return 0;
+        var power = Owner.Creature.GetPower<InherentMemoryPower>();
+        return power != null  ? power.Amount * 4 : 0;
     }
 }
