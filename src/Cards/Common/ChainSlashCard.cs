d616bc835ea30a84567762689c931eacccee07d1
SKY <463731963@qq.com>
2026-09-26 18:25:15 +0800

    update

---
+++ src/Cards/Common/ChainSlashCard.cs
@@ -0,0 +1,53 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Common;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class ChainSlashCard : ModCardTemplate
+{
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(7, ValueProp.Move)];
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
+    
+    public ChainSlashCard() : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this,cardPlay)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3);
+    }
+
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource,
+        CardPlay? cardPlay)
+    {
+        if (cardSource != this) return 0;
+        var power = Owner.Creature.GetPower<InherentMemoryPower>();
+        if (power != null)
+        {
+            return power.Amount;
+        }
+
+        return 0;
+    }
+}
---
+++ src/Cards/Rare/DawnglowCard.cs
@@ -0,0 +1,53 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Rare;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class DawnglowCard : ModCardTemplate
+{
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(5, ValueProp.Move)];
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
+    
+    public DawnglowCard() : base(2, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(5);
+    }
+
+    public override decimal ModifyDamageAdditive(Creature? target, decimal amount, ValueProp props, Creature? dealer, CardModel? cardSource,
+        CardPlay? cardPlay)
+    {
+        if (cardSource != this) return 0;
+        var power = Owner.Creature.GetPower<InherentMemoryPower>();
+        if (power != null)
+        {
+            return power.Amount * 5;
+        }
+
+        return 0;
+    }
+}
