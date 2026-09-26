91709c88f42983f8ae969544de272b65c8af4ebb
SKY <463731963@qq.com>
2026-09-26 22:19:01 +0800

    update

---
+++ src/Cards/Common/RadiantShieldCard.cs
@@ -0,0 +1,43 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Common;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class RadiantShieldCard : ModCardTemplate
+{
+    public override bool GainsBlock => true;
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Defend];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(7, ValueProp.Move)];
+    public override CardAssetProfile AssetProfile => new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+    
+    public RadiantShieldCard() : base(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3);
+    }
+
+    public override decimal ModifyBlockAdditive(Creature target, decimal block, ValueProp props, CardModel? cardSource, CardPlay? cardPlay)
+    {
+        if (cardSource != this) return 0;
+        var power = Owner.Creature.GetPower<InherentMemoryPower>();
+        return power != null ? power.Amount : 0;
+    }
+}
