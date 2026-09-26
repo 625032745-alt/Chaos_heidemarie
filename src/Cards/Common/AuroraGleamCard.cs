eb779a662b29cfecc09bfd93dbca25f4890fcc17
SKY <463731963@qq.com>
2026-09-26 19:31:33 +0800

    update

---
+++ src/Cards/Common/AuroraGleamCard.cs
@@ -0,0 +1,45 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Common;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class AuroraGleamCard : ModCardTemplate
+{
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(3, ValueProp.Move)];
+
+    public AuroraGleamCard() : base(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        var power = Owner.Creature.GetPower<InherentMemoryPower>();
+        var count = 1;
+        if (power != null)
+        {
+            count = power.Amount;
+        }
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this, cardPlay)
+            .Targeting(cardPlay.Target)
+            .WithHitCount(count)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2);
+    }
+}
