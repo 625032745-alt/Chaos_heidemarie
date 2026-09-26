b6f4fe428c5b29acbaabaac00c59663b99353317
SKY <463731963@qq.com>
2026-09-26 14:00:16 +0800

    update

---
+++ src/Cards/Rare/DawnshatterCard.cs
@@ -0,0 +1,34 @@
+﻿using ChaosHeidemarie.Content;
+using ChaosHeidemarie.Power;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Cards.Rare;
+
+[RegisterCard(typeof(HeidemarieCardPool))]
+public class DawnshatterCard : ModCardTemplate
+{
+    public override CardAssetProfile AssetProfile =>
+        new(PortraitPath: $"res://ArtWorks/images/cards/{GetType().Name}.png");
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(15, ValueProp.Move)];
+    
+    public DawnshatterCard() : base(3, CardType.Power, CardRarity.Rare, TargetType.Self)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<DawnshatterPower>(choiceContext, Owner.Creature,
+            DynamicVars.Damage.BaseValue, Owner.Creature, null);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(7);
+    }
+}
---
+++ src/Power/DawnshatterPower.cs
@@ -0,0 +1,34 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ChaosHeidemarie.Power;
+
+[RegisterPower]
+public class DawnshatterPower : ModPowerTemplate
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    public override PowerAssetProfile AssetProfile => new(
+        IconPath: "res://ArtWorks/images/power/DawnshatterPower_Small.png",
+        BigIconPath: "res://ArtWorks/images/power/DawnshatterPower_Big.png"
+    );
+
+    public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power,
+        decimal amount, Creature? applier,
+        CardModel? cardSource)
+    {
+        if (power.Amount == 10)
+        {
+            await DamageCmd.Attack(Amount)
+                .FromCard(cardSource, null)
+                .TargetingAllOpponents(cardSource.CombatState)
+                .WithHitFx("vfx/vfx_attack_slash")
+                .Execute(choiceContext);
+        }
+    }
+}
